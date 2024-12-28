2c475699ad1c3b4fa51f133e78eba328e6eaa568
Jason Specland <jayspec@gmail.com>
2024-12-27 21:41:38 -0500

    added function prototypes

--- src/shot.c
+++ src/shot.c
@@ -1,19 +1,41 @@
 #include "globals.h"
 
 #include <stdint.h>
+#include <stddef.h>
 #include <cx16.h>
-#include <stdio.h>
 
 #define SHOT_SPEED 7
 #define SHOT_FRAME_DELAY 10
 
 uint8_t nextShotCountdown = SHOT_FRAME_DELAY;
 
+void advance_shot_at_index(uint8_t attr_index);
+void fire_shot();
+
+void handle_shots() {
+
+    for (uint8_t i = 0; i < MAX_SHOTS; i++) {
+        advance_shot_at_index(i);
+    }
+
+    if (nextShotCountdown > 0) {
+        nextShotCountdown--;
+    }
+
+    if ((nextShotCountdown == 0) && (!(g_joystick_state.data0 & JOY_BTN_B_MASK))) {
+        if (g_numLiveShots < MAX_SHOTS) {
+            fire_shot();
+            nextShotCountdown = SHOT_FRAME_DELAY;
+            g_numLiveShots++;
+        }
+    }
+
+}
+
 void advance_shot_at_index(uint8_t attr_index) {
     shot_t *shot = &g_liveShots[attr_index];
 
     if (shot->isActive) {
-        //printf("moving active shot %d", attr_index);
         VERA.address = (shot->attr_address + 2) & 0xffff;
         VERA.address_hi = (((shot->attr_address + 2) >> 16) | VERA_INC_1);
 
@@ -54,24 +76,3 @@ void fire_shot() {
 
 }
 
-void handle_shots() {
-
-    for (uint8_t i = 0; i < MAX_SHOTS; i++) {
-        advance_shot_at_index(i);
-    }
-
-    if (nextShotCountdown > 0) {
-        nextShotCountdown--;
-    }
-
-    //printf("ns countdown: %d, joystick state: %lx\n", nextShotCountdown, g_joystick_state);
-    if ((nextShotCountdown == 0) && (!(g_joystick_state.data0 & JOY_BTN_B_MASK))) {
-        //printf("fire\n");
-        if (g_numLiveShots < MAX_SHOTS) {
-            fire_shot();
-            nextShotCountdown = SHOT_FRAME_DELAY;
-            g_numLiveShots++;
-        }
-    }
-
-}
--- src/sprite_load.c
+++ src/sprite_load.c
@@ -10,6 +10,42 @@
 #define SPRITE_SIZE 512 // 32x32 pixels, 4bpp
 #define PALETTE_LINE_SIZE 16 * 2 // 2 bytes per entry, 16 entries per line
 
+unsigned long load_objects_to_vram_at_index(const char * filename, unsigned long base_vram_addr, uint16_t object_size, uint8_t object_index);
+unsigned long load_sprite_to_vram_at_index(const char* filename, uint8_t sprite_index);
+unsigned long load_palette_to_vram_at_index(const char* filename, uint8_t palette_index);
+unsigned long set_sprite_attributes_at_index(uint8_t attr_index, unsigned long vram_addr, int16_t xPos, int16_t yPos, uint8_t zDepth);
+
+void load_sprites() {
+
+    vera_sprites_enable(1);
+
+    // load the sprites into vram,
+    // Then set their attributes
+
+    // load the palette for the hero ships
+    // load at index 1, because (for now) we don't want to blow away the palette for the background
+    unsigned long palette_vram_addr = load_palette_to_vram_at_index("assets/heros.pal", 1);
+    // There are 4 ships on the sprite sheet
+    unsigned long hero_ships_vram_addr = load_sprite_to_vram_at_index("assets/heros.img", 0);
+    // We're just going to worry about the first ship for now
+    set_sprite_attributes_at_index(0, hero_ships_vram_addr, g_shipXPos, g_shipYPos, 3);
+
+    // Load the shots.  There can be multiple shots, all pointing to the same image in vram
+    unsigned long shot_vram_addr = load_sprite_to_vram_at_index("assets/shot.img", 4);
+    for (uint8_t i = 0; i < MAX_SHOTS; i++) {
+        // i + 1 because the ship is at 0
+        unsigned long sprite_attr_addr = set_sprite_attributes_at_index(i + 1, shot_vram_addr, 0, 0, 0);
+        
+        shot_t shot;
+        shot.attr_address = sprite_attr_addr;
+        shot.isActive = 0;
+        shot.xPos = 0;
+        shot.yPos = 0;
+
+        g_liveShots[i] = shot;
+    }
+}
+
 unsigned long load_objects_to_vram_at_index(const char* filename, unsigned long base_vram_addr, uint16_t object_size, uint8_t object_index) {
     unsigned long vram_addr = (base_vram_addr + (object_size * object_index));
 
@@ -65,35 +101,4 @@ unsigned long set_sprite_attributes_at_index(uint8_t attr_index, unsigned long v
     return attr_addr;
 }
 
-void load_sprites() {
-
-    vera_sprites_enable(1);
-
-
-    // load the sprites into vram,
-    // Then set their attributes
-
-    // load the palette for the hero ships
-    // load at index 1, because (for now) we don't want to blow away the palette for the background
-    unsigned long palette_vram_addr = load_palette_to_vram_at_index("assets/heros.pal", 1);
-    // There are 4 ships on the sprite sheet
-    unsigned long hero_ships_vram_addr = load_sprite_to_vram_at_index("assets/heros.img", 0);
-    // We're just going to worry about the first ship for now
-    set_sprite_attributes_at_index(0, hero_ships_vram_addr, g_shipXPos, g_shipYPos, 3);
-
-    // Load the shots.  There can be multiple shots, all pointing to the same image in vram
-    unsigned long shot_vram_addr = load_sprite_to_vram_at_index("assets/shot.img", 4);
-    for (uint8_t i = 0; i < MAX_SHOTS; i++) {
-        // i + 1 because the ship is at 0
-        unsigned long sprite_attr_addr = set_sprite_attributes_at_index(i + 1, shot_vram_addr, 0, 0, 0);
-        
-        shot_t shot;
-        shot.attr_address = sprite_attr_addr;
-        shot.isActive = 0;
-        shot.xPos = 0;
-        shot.yPos = 0;
-
-        g_liveShots[i] = shot;
-    }
-}
 
